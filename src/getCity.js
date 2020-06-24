592a8fa72eb5907a97308ffb5f918579577f5dcd
ZAFRUL HASSAN <zafrul@MDs-MacBook-Pro.local>
2020-06-24 19:43:51 +0200

    show html page

--- src/getCity.js
+++ src/getCity.js
@@ -1,16 +1,15 @@
 import axios from 'axios';
+import weatherPage from './weatherPage';
 
 async function getCity() {
   const res = { data: { city: 'Berlin' } }; //await axios.get('http://ip-api.com/json');
   return res.data.city;
 }
-
+let city = '';
 async function cityWoeId() {
-  const city = await getCity();
+  city = await getCity();
 
-  const urlProxy =
-    'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search?query=' +
-    city;
+  const urlProxy = `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search?query=${city}`;
   const res = await axios.get(urlProxy);
   return res.data[0].woeid;
 }
@@ -23,14 +22,13 @@ async function cityWeather() {
     new Date().getMonth().toString() +
     '/' +
     new Date().getDate().toString();
-  const urlProxy =
-    'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/' +
-    woeid +
-    '/' +
-    date;
+
+  const urlProxy = `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/${date}/`;
 
   const res = await axios.get(urlProxy);
-  return res.data[0];
+  const weatherRecords = JSON.parse(JSON.stringify(res.data));
+
+  return weatherPage(city, weatherRecords);
 }
 
 export default cityWeather;
--- src/weatherPage.js
+++ src/weatherPage.js
@@ -1,11 +1,49 @@
-async function htmlTable(params) {}
+const weather = {
+  applicable_date: '',
+  weather_state_name: '',
+  weather_state_abbr: '',
+  wind_direction: '',
+  the_temp: '',
+};
 
-async function showCity(params) {}
+function header(city) {
+  return `
+  <div class="div-table-row">
+        <h3>City ${city}</h3>
+        </div>
+        <div class="div-table-row"> 
+        <div class="div-table-col"><h5>Date</h5></div>
+        <div class="div-table-col"><h5>Weather conditions</h5></div>
+        <div class="div-table-col"><h5>Wind direction</h5></div>
+        <div class="div-table-col"><h5>Temparature (centigrade)</h5></div>             
+        </div>`;
+}
 
-async function showDate(params) {}
+function records(weatherRecords) {
+  let weatherTableRows = ``;
+  if (Array.isArray(weatherRecords) && weatherRecords.length > 1) {
+    weatherRecords.forEach((weather) => {
+      weatherTableRows += rowHtml(weather);
+    });
+  }
+  return weatherTableRows;
+}
 
-async function showWeatherCondition(params) {}
+function rowHtml(weather) {
+  return `
+  <div class="div-table-row">
+    <div class="div-table-col">${weather.applicable_date}</div>
+    <div class="div-table-col">${weather.weather_state_name}, ${weather.weather_state_abbr}</div>
+    <div class="div-table-col">${weather.wind_direction}</div>
+    <div class="div-table-col">${weather.temparature}</div>
+    </div>`;
+}
 
-async function showTemparature(params) {}
+function weatherPage(city, weatherRecords) {
+  return `<div class="div-table">
+              ${header(city)}
+              ${records(weatherRecords)}
+            </div>`;
+}
 
-async function showWindDirection(params) {}
+export default weatherPage;
